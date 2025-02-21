9e932fc0b3c069e1107e3a6928ca8fde1c80740c
root <root@DESKTOP-A7TPCGN.>
2025-02-21 20:06:16 +1030

    Player & Circle class

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from ctypes.wintypes import RGB
+from player import Player
 import pygame
 from pygame.locals import *
 from constants import *
 
 def main():
-    game = pygame.init()
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    dt = 0 # delta time
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     
     # GAME LOOP
     while True:
@@ -16,8 +20,11 @@ def main():
         
         # BACKGROUND BLACK
         screen.fill(RGB(0,0,0))
+        player.draw(screen)
         # DISPLAY GAME
         pygame.display.flip()
+        dt = game_clock.tick(60)/1000
+        
     print("Starting Asteroids!")
     print("Screen width:", constants.SCREEN_WIDTH)
     print("Screen height:", constants.SCREEN_HEIGHT)
